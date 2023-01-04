8567669718046470b725bb53ea8fb3d0991eb9a4
vanessaffd <121908711+vanessaffd@users.noreply.github.com>
2023-01-03 21:18:33 -0300

    Add files via upload
    Arquivo principal

---
+++ vanessaaula01.py
@@ -0,0 +1,19 @@
+'''print("Olá Mundo!")
+
+
+desejo = input("Qual seu desejo? ")
+
+print(f"seu desejo é {desejo}")'''
+
+nome = input("Olá qual seu nome? ")
+
+peso = float(input( "Qual seu peso? "))
+
+altura = float(input("Qual sua altura? "))
+
+
+imc= peso/(altura*altura)
+
+print(f"{nome}, seu imc é {imc}.")
+
+input("Gostou? ")
